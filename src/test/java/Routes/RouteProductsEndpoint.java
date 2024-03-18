9b3d4550b9f783c4c0200781df328687d55906a1
Igal_Kroif <igal2344@gmail.com>
2024-03-18 22:04:09 +0200

    --added store urls

--- src/test/java/Routes/RouteProductsEndpoint.java
+++ src/test/java/Routes/RouteProductsEndpoint.java
@@ -4,5 +4,7 @@ import static Routes.BaseRoute.baseUrl;
 
 public class RouteProductsEndpoint {
     public static final String allProducts = baseUrl + "/products";
-    public static final String singleProduct = allProducts + "/{id}";
+    public static final String singleProduct = baseUrl + "/products" + "/{id}";
+    public static final String allCategories = baseUrl + "/products/categories";
+    public static final String specificCategory = baseUrl + "/products/category" + "/{category}";
 }

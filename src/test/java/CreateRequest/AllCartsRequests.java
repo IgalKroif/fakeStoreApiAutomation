b9f810a9875a4348d17c73802793ac69b1e9413a
Igal_Kroif <igal2344@gmail.com>
2024-03-23 17:23:24 +0200

    -- added some framework assertion and validation methods. -- added a CONSTANT_VALIDATION.java interface -- created SanityTests.java class for the aforementioned. -- created SpecifyRequest.java and SpecifyResponse.java classes for specifications. -- asserted all carts and products

--- src/test/java/CreateRequest/AllCartsRequests.java
+++ src/test/java/CreateRequest/AllCartsRequests.java
@@ -8,12 +8,21 @@ import org.junit.jupiter.api.Tags;
 import utils.reqSpecification.REQ_SPEC;
 import utils.resSpecification.RES_SPEC;
 
+import java.util.concurrent.TimeUnit;
+
 import static Routes.RouteCartEndPoint.allCarts;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
 
 @Slf4j
 public class AllCartsRequests implements REQ_SPEC, RES_SPEC {
 
+    /**
+     * This method tests all carts by sending a GET request to the specified endpoint.
+     * It asserts that the response time is less than 3 seconds and the status code is 200.
+     *
+     * @return The response object.
+     */
     @Tags({@Tag("Carts"), @Tag("SanityTest")})
     @DisplayName("assert items in cart")
     public Response testAllCarts() {
@@ -22,7 +31,9 @@ public class AllCartsRequests implements REQ_SPEC, RES_SPEC {
                 .when()
                 .get(allCarts);
         response.then().spec(getResponseSpec());
+        response.then().time(lessThan(3L), TimeUnit.SECONDS);
         response.then().statusCode(200);
+
         return response;
     }
 }
